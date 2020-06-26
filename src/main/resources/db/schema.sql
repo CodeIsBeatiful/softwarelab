e361356e3523ed78255f2ab60fc2f35685abb56c
blackstar-baba <535650957@qq.com>
2020-06-26 23:40:04 +0800

    something lost

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,53 +1,65 @@
-create table public.sys_user
+CREATE TABLE IF NOT EXISTS sys_user
 (
-    id varchar(36) not null
+    id          varchar(36) not null
         constraint sys_user_pk
             primary key,
-    username varchar(50),
-    password varchar(100),
-    mail varchar(100),
+    username    varchar(50),
+    password    varchar(100),
+    mail        varchar(100),
     create_time timestamp,
     update_time timestamp,
-    status integer
+    status      integer
 );
 
-alter table public.sys_user owner to postgres;
-
-create table public.instance
+CREATE TABLE IF NOT EXISTS instance
 (
-    id varchar(36) not null
+    id              varchar(36) not null
         constraint instance_pk
             primary key,
-    user_id varchar(36),
-    name varchar(100),
-    description text,
-    create_time timestamp,
-    update_time timestamp,
-    status integer,
-    app_id varchar(36),
+    user_id         varchar(36),
+    name            varchar(100),
+    description     text,
+    create_time     timestamp,
+    update_time     timestamp,
+    status          integer,
+    app_id          varchar(36),
     additional_info varchar
 );
 
-alter table public.instance owner to postgres;
 
-create table public.app
+CREATE TABLE IF NOT EXISTS app
 (
-    id varchar(36) not null
+    id              varchar(36) not null
         constraint app_pk
             primary key,
-    author varchar(36),
-    type varchar(50),
-    name varchar(100),
-    description text,
-    create_time timestamp,
-    update_time timestamp,
-    status integer,
+    author          varchar(36),
+    type            varchar(50),
+    name            varchar(100),
+    description     text,
+    create_time     timestamp,
+    update_time     timestamp,
+    status          integer,
     additional_info varchar,
-    logo bytea,
-    logo_type varchar(10)
+    logo            bytea,
+    logo_type       varchar(10)
 );
 
-alter table public.app owner to postgres;
+
+CREATE TABLE IF NOT EXISTS file
+(
+    id          varchar(36) not null
+        constraint file_pk
+            primary key,
+    name        varchar(200),
+    type        varchar(10),
+    data        bytea,
+    create_time timestamp,
+    update_time timestamp,
+    status      integer
+);
+
+
+
 
 
 
---
+++ src/test/java/com/blackstar/softwarelab/AbstractBaseTest.java
@@ -0,0 +1,17 @@
+package com.blackstar.softwarelab;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+@Slf4j
+public abstract class AbstractBaseTest {
+}
