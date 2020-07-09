137d0567380c90ab5bc3df132118ec485bc1b18b
blackstar-baba <535650957@qq.com>
2020-07-09 23:29:04 +0800

    add portutil to test port

---
+++ src/main/java/com/blackstar/softwarelab/util/PortUtil.java
@@ -0,0 +1,34 @@
+package com.blackstar.softwarelab.util;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * @author blackstar
+ */
+public class PortUtil {
+
+    /**
+     * test port is not bind,if bind, return false
+     * @param port
+     * @return
+     */
+    public static boolean canUse(int port){
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            return true;
+        } catch (IOException e) {
+            //do nothing
+        } finally {
+            if ( serverSocket != null ){
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
+}
