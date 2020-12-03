77d6c1882167b8b8abadf2cb214d1380412c99a4
blackstar-baba <535650957@qq.com>
2020-12-03 23:35:20 +0800

    update pom

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
   publicPath: '/',
-  outputDir: 'dist',
+  outputDir: 'target/generated-resources/public',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
